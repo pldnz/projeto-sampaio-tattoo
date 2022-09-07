c98eb5dd9a0e66d10e2f947627f425d3c26fe13a
pldnz <paulosouzadiniz@hotmail.com>
2022-09-07 20:41:40 -0300

    refatorado nav, main e portfolio com tailwind

--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 2,
-    nav: false,
-    items: 3,
-    autoplay: true,
-    autoplayTimeout: 1500,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      400: {
-        items: 2,
-      },
-      600: {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 2,
+        nav: false,
         items: 3,
-      },
-    },
-  });
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            400: {
+                items: 2,
+            },
+            600: {
+                items: 3,
+            },
+        },
+    });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./style/*.css", "index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./style/*.css", "index.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                times: ['"Times New Roman"', "Times", "serif"],
+                montserrat: ["Montserrat", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
 };
